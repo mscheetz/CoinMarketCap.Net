de78f4fc4da52fe03208f59b06d4595cea84427b
Matt Scheetz <mfscheetz@gmail.com>
2018-11-29 22:44:05 -0500

    new tests

--- CoinMarketCap.Net/CoinMarketCap.Net.Tests/CoinMarketCapRepositoryTests.cs
+++ CoinMarketCap.Net/CoinMarketCap.Net.Tests/CoinMarketCapRepositoryTests.cs
@@ -1,6 +1,7 @@
 using CoinMarketCap.Net.DataAccess;
 using CoinMarketCap.Net.DataAccess.Interfaces;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace CoinMarketCap.Net.Tests
@@ -20,11 +21,51 @@ namespace CoinMarketCap.Net.Tests
         }
 
         [Fact]
+        public void GetMetadataId_Test()
+        {
+            var id = 1;
+
+            var data = repo.GetMetadata(id).Result;
+
+            Assert.NotNull(data);
+        }
+
+        [Fact]
         public void GetMetadataSymbol_Test()
         {
             var symbol = "BTC";
 
             var data = repo.GetMetadata(symbol).Result;
+
+            Assert.NotNull(data);
+        }
+
+        [Fact]
+        public void GetMetadataIds_Test()
+        {
+            var ids = new List<int> { 1, 2 };
+
+            var data = repo.GetMetadata(ids).Result;
+
+            Assert.NotNull(data);
+        }
+
+        [Fact]
+        public void GetMetadataSymbols_Test()
+        {
+            var symbol = "BTC,ETH";
+
+            var data = repo.GetMetadata(symbol).Result;
+
+            Assert.NotNull(data);
+        }
+
+        [Fact]
+        public void GetMetadataSymbolList_Test()
+        {
+            var symbols = new List<string> { "BTC", "ETH" };
+
+            var data = repo.GetMetadata(symbols).Result;
 
             Assert.NotNull(data);
         }
